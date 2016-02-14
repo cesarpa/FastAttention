ca81ff836a0501edae3cfcbce72decb9e1d663dd
cesarpa <cesar.1023@hotmail.com>
2016-02-13 22:13:31 -0500

    cesar commmit cs

---
+++ Assets/scripts/ClienteControlador.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class ClienteControlador : MonoBehaviour {
+    public float velocidadEnX = 0.1f;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, 0, velocidadEnX);
+    }
+
+   
+}

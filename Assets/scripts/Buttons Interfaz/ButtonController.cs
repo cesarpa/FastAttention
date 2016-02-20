067621af1d7faf104dd6b56aecf467986bf9989c
Juan Felipe Arias <juantrullo@gmail.com>
2016-02-20 13:10:39 -0500

    jfa-2016-02-20 - Se crea scene de pruebas para mesa con objetos 2d

---
+++ Assets/scripts/Buttons Interfaz/ButtonController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Detection ();
+	}
+
+	void Detection()
+	{
+		/*if (Input.GetMouseButtonDown (0)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			Transform select = GameObject.FindWithTag ("select").transform;
+			if (Physics.Raycast (ray, hit, 100.0f)) {
+				select.tag = "none";
+				hit.collider.transform.tag = "select";
+			}
+		}*/
+
+		if (Input.GetMouseButtonDown(0)){ // if left button pressed...
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit)){
+				// the object identified by hit.transform was clicked
+				// do whatever you want
+				Debug.Log("Realizo Click");
+			}
+		}
+
+	}
+
+	void OnMouseDown()
+	{
+		if(Input.GetKey("mouse 0")){
+			Debug.Log ("Click Objeto");
+		}
+	}
+}
---
+++ Assets/scripts/Cliente/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cliente : MonoBehaviour {
+
+	public float velocityCliente;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveClient ();
+	}
+
+	void MoveClient()
+	{
+		transform.Translate ((velocityCliente*Time.deltaTime)*-1,0,0);	
+	}
+}
